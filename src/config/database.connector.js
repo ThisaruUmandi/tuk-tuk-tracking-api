21b07e88146d30ed1fc64a02ec48c6195fb16dff
thisarudsilva@gmail.com <thisarudsilva1@gmail.com>
2026-04-08 11:05:16 +0530

    fix database issue

--- src/config/database.connector.js
+++ src/config/database.connector.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD || ""),
+  password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
 });
 
--- src/models/province.model.js
+++ src/models/province.model.js
@@ -1,4 +1,4 @@
-import databaseConnection from "../config/database.connection.js";
+import databaseConnection from "../config/database.connector.js";
 
 const getAllProvinces = async () => {
   const query = `
