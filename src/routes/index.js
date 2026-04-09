46273fccd209f98fd83847df98315b95761a65b5
thisarudsilva@gmail.com <thisarudsilva1@gmail.com>
2026-04-09 23:06:52 +0530

    Add validations, filtering and postman testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import healthRoutes from "./health.route.js";
 import provinceRoutes from "./province.routes.js";
 import districtRoutes from "./district.routes.js";
+import policeStationRoutes from "./policeStation.routes.js";
 
 const router = express.Router();
 
 router.use("/health", healthRoutes);
 router.use("/provinces", provinceRoutes);
 router.use("/districts", districtRoutes);
+router.use("/police-stations", policeStationRoutes);
 
 export default router;
--- src/routes/policeStation.routes.js
+++ src/routes/policeStation.routes.js
@@ -6,16 +6,24 @@ import {
   updatePoliceStation,
   deletePoliceStation,
 } from "../controllers/policeStation.controller.js";
+import {
+  validatePoliceStationId,
+  validatePoliceStationQuery,
+  validateCreatePoliceStation,
+  validateUpdatePoliceStation,
+} from "../validations/policeStation.validation.js";
 
 const router = express.Router();
 
-router.get("/", getAllPoliceStations);
-router.get("/:id",getPoliceStationById);
-router.post("/", createPoliceStation);
+router.get("/", validatePoliceStationQuery, getAllPoliceStations);
+router.get("/:id", validatePoliceStationId, getPoliceStationById);
+router.post("/", validateCreatePoliceStation, createPoliceStation);
 router.put(
   "/:id",
+  validatePoliceStationId,
+  validateUpdatePoliceStation,
   updatePoliceStation
 );
-router.delete("/:id", deletePoliceStation);
+router.delete("/:id", validatePoliceStationId, deletePoliceStation);
 
 export default router;
